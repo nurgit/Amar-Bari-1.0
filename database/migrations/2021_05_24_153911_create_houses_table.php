83f1cb65822e214dae7450651c639854ee600443
nurgit <nurmohammad.aiub@gmail.com>
2021-05-24 22:56:32 +0600

    housse migration and model

---
+++ database/migrations/2021_05_24_153911_create_houses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHousesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('houses', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('holding_no');
+            $table->string('road_no');
+            $table->string('city');
+            $table->string(' district'); 
+            $table->integer('status')->default(0);//0=inactive ,1= active 
+            $table->integer('dlt')->default(1);// 0=dlt , 1=not dlt
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('houses');
+    }
+}
