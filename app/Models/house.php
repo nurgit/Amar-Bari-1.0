bc716979bad7ecaf614fc38c82f2f942016a91be
nurgit <nurmohammad.aiub@gmail.com>
2021-05-24 23:01:15 +0600

    housse  model

--- app/Models/house.php
+++ app/Models/house.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class house extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'holding_no',
+        'road_no',
+        'city',
+        'district',
+        'status',
+        'dlt',
+       // 'owner_username',
+        
+    ];
 }
